f002f13726e09e3f766cd2fbf71b9db9f1549878
하물 <108536712+AnnieHa1002@users.noreply.github.com>
2025-01-29 12:53:42 +0900

    [add] Initial commit

---
+++ annie/src/main/java/com/annies_note/annie/AnniesNoteApplication.java
@@ -0,0 +1,13 @@
+package com.annies_note.annie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnniesNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnniesNoteApplication.class, args);
+	}
+
+}
---
+++ annie/src/test/java/com/annies_note/annie/AnniesNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annies_note.annie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnniesNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
